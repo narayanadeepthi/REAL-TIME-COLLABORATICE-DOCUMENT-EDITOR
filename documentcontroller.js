ad809c116f5fabb71a11aa337db75bbd4160fcef
narayanadeepthi <deepthirsd35@gmail.com>
2025-05-25 16:05:28 +0530

    Create documentcontroller.js

---
+++ documentcontroller.js
@@ -0,0 +1,19 @@
+class DocumentController {
+    async createDocument(req, res) {
+        // Logic to create a new document
+    }
+
+    async getDocument(req, res) {
+        // Logic to retrieve a document by ID
+    }
+
+    async updateDocument(req, res) {
+        // Logic to update a document by ID
+    }
+
+    async deleteDocument(req, res) {
+        // Logic to delete a document by ID
+    }
+}
+
+export default DocumentController;
